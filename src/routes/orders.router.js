e9c8d0af672b272cff1bb6a7ef85f331a6b8029b
wltjsl <ljss7291@gmail.com>
2024-02-26 19:19:22 +0900

    테스트 용으로 변경했던 사항 복원

--- src/routes/orders.router.js
+++ src/routes/orders.router.js
@@ -6,19 +6,14 @@ const router = express.Router();
 
 const ordersController = new OrdersController();
 
-// 테스트용
-router.get('/', (req, res) => ordersController.getAllOrders(req, res));
-router.get('/:orderId', (req, res) => ordersController.getOrderById(req, res));
-router.patch('/:orderId', (req, res) => ordersController.updateOrder(req, res));
-
-// router.get('/', authMiddleware, (req, res) =>
-//   ordersController.getAllOrders(req, res)
-// );
-// router.get('/:orderId', authMiddleware, (req, res) =>
-//   ordersController.getOrderById(req, res)
-// );
-// router.patch('/:orderId', authMiddleware, (req, res) =>
-//   ordersController.updateOrder(req, res)
-// );
+router.get('/', authMiddleware, (req, res) =>
+  ordersController.getAllOrders(req, res)
+);
+router.get('/:orderId', authMiddleware, (req, res) =>
+  ordersController.getOrderById(req, res)
+);
+router.patch('/:orderId', authMiddleware, (req, res) =>
+  ordersController.updateOrder(req, res)
+);
 
 export default router;
