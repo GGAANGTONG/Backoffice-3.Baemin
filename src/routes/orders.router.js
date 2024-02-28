80a22285a587d87a5ae274022af30a20e1134b65
wltjsl <ljss7291@gmail.com>
2024-02-28 11:04:01 +0900

    라우터 커밋

--- src/routes/orders.router.js
+++ src/routes/orders.router.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
-import OrdersController from '../controllers/orders.controller.js';
+import { dataSource } from '../typeorm/index.js';
+import { OrdersController } from '../controllers/orders.controller.js';
+import { OrdersRepository } from '../repositories/orders.repository.js';
+import { OrdersService } from '../services/orders.service.js';
 
 const router = express.Router();
 
-const ordersController = new OrdersController();
+const ordersRepository = new OrdersRepository(dataSource);
+const ordersService = new OrdersService(ordersRepository);
+const ordersController = new OrdersController(ordersService);
 
 router.get('/', authMiddleware, (req, res) =>
   ordersController.getAllOrders(req, res)
