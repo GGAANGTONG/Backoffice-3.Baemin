101d1647111f47139364bf0c358ffa6408d00154
wltjsl <ljss7291@gmail.com>
2024-02-28 19:25:05 +0900

    배달 완료 시 트랜잭션 적용

--- src/repositories/orders.repository.js
+++ src/repositories/orders.repository.js
@@ -36,14 +36,35 @@ export class OrdersRepository {
   }
 
   async completeOrder(orderId, userId) {
-    const order = await this.dataSource.getRepository('orders').findOne({
-      where: { orderId: orderId },
-    });
-    const user = await this.dataSource.getRepository('users').findOne({
-      where: { userId: userId },
-    });
-    user.point = user.point - 0 + (order.totalPrice - 0);
-    await this.dataSource.getRepository('users').save(user);
-    return user.point;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const order = await queryRunner.manager.findOne('orders', {
+        where: {
+          orderId: orderId,
+        },
+      });
+      const user = await queryRunner.manager.findOne('users', {
+        where: {
+          userId: userId,
+        },
+      });
+      if (!order || !user) {
+        throw new Error('회원이나 주문이 존재하지 않습니다.');
+      }
+
+      user.point = user.point - 0 + (order.totalPrice - 0);
+
+      await queryRunner.manager.save('users', user);
+      await queryRunner.commitTransaction();
+
+      return user.point;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
