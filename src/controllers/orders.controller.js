609b5b77d0895b296fe33577f864d4a3a6c4305b
wltjsl <ljss7291@gmail.com>
2024-02-28 11:02:11 +0900

    컨트롤러 커밋

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -1,15 +1,16 @@
-import OrdersService from '../services/orders.service.js';
-
-class OrdersController {
+export class OrdersController {
+  constructor(ordersService) {
+    this.ordersService = ordersService;
+  }
   getAllOrders = async (req, res, next) => {
     try {
       const user = res.locals.user;
 
-      if (user.role !== 'owner') {
+      if (user.role !== '식당 주인') {
         return res.status(400).json({ errorMessage: '접근 권한이 없습니다.' });
       }
 
-      const storeId = await OrdersService.findStoreId(user.email);
+      const storeId = await this.ordersService.findStoreId(user.email);
 
       if (!storeId) {
         return res
@@ -17,7 +18,7 @@ class OrdersController {
           .json({ errorMessage: '계정으로 등록된 가게가 없습니다.' });
       }
 
-      const orders = await OrdersService.getAllOrders(storeId);
+      const orders = await this.ordersService.getAllOrders(storeId);
 
       if (!orders.length) {
         return res.status(404).json({ errorMessage: '주문이 없습니다.' });
@@ -33,12 +34,12 @@ class OrdersController {
     try {
       const user = res.locals.user;
 
-      if (user.role !== 'owner') {
+      if (user.role !== '식당 주인') {
         return res.status(400).json({ errorMessage: '접근 권한이 없습니다.' });
       }
 
       const { orderId } = req.params;
-      const order = await OrdersService.getOrderById(orderId);
+      const order = await this.ordersService.getOrderById(orderId);
 
       if (!order) {
         return res.status(404).json({ errorMessage: '해당 주문이 없습니다.' });
@@ -54,40 +55,60 @@ class OrdersController {
     try {
       const user = res.locals.user;
 
-      if (user.role !== 'owner') {
+      if (user.role !== '식당 주인') {
         return res.status(400).json({ errorMessage: '접근 권한이 없습니다.' });
       }
 
       const { orderId } = req.params;
       const { status } = req.body;
 
-      const order = await OrdersService.getOrderById(orderId);
+      if (!status) {
+        return res
+          .status(400)
+          .json({ errorMessage: '변경할 상태가 입력되지 않았습니다.' });
+      }
+
+      const order = await this.ordersService.getOrderById(orderId);
 
       if (!order) {
         return res
           .status(404)
           .json({ errorMessage: '해당 주문이 존재하지 않습니다.' });
       }
-      if (order.status === '배달 완료')
-        if (!status) {
-          return res
-            .status(400)
-            .json({ errorMessage: '이미 배달이 완료된 주문입니다.' });
-        }
 
-      const updatedOrder = await OrdersService.updateOrder(orderId, status);
+      const storeId = await this.ordersService.findStoreId(user.email);
 
-      if (updatedOrder.status === '배달 완료') {
-        await OrdersService.completeOrder(orderId, user.userId);
+      if (storeId !== order.storeId) {
+        return res
+          .status(400)
+          .json({ errorMessage: '해당 가게의 주문이 아닙니다.' });
       }
 
-      return res
-        .status(200)
-        .json({ data: updatedOrder, message: '성공적으로 변경되었습니다.' });
+      if (order.status === '배달 완료') {
+        ('이미 배달이 완료된 주문입니다.');
+      }
+
+      const updatedOrder = await this.ordersService.updateOrder(
+        orderId,
+        status
+      );
+
+      if (updatedOrder.status === '배달 완료') {
+        const resultPoint = await this.ordersService.completeOrder(
+          orderId,
+          user.userId
+        );
+        return res.status(200).json({
+          message: `성공적으로 배달 완료 처리되었습니다. 현재 포인트: ${resultPoint}`,
+        });
+      }
+
+      return res.status(200).json({
+        data: updatedOrder,
+        message: '상태가 성공적으로 변경되었습니다.',
+      });
     } catch (error) {
       next(error);
     }
   };
 }
-
-export default OrdersController;
