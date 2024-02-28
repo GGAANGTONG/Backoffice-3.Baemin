dedbfd67823b53ab44b4f87246b483e42a8a6be2
cocojerry1 <91053150+cocojerry1@users.noreply.github.com>
2024-02-28 22:34:31 +0900

    Update reviews.router.js
    수정

--- src/routes/reviews.router.js
+++ src/routes/reviews.router.js
@@ -1,34 +1,23 @@
 import express from 'express';
 import authMiddleware from '../../middleware/jwt-validate.middleware.js';
-import { ReviewsController } from '../controllers/reviewscontroller.js';
-import { ReviewsRepository } from '../repositories/reviewsrepository.js';
-import { ReviewsService } from '../services/reviewsservice.js';
+import  ReviewsRepository from '../repositories/reivewsrepsitory.js';
+import  ReviewsController  from '../controllers/reviewscontroller.js';
+import  ReviewsService  from '../services/reviewsservice.js';
 import { dataSource } from '../typeorm/index.js';
 
 const router = express.Router();
 
-const reviewsRepository = new ReviewsRepository(dataSource);
+const reviewRepository = new ReviewsRepository(dataSource);
 
-const reviewsService = new ReviewsService(reviewsRepository);
+const reviewsService = new ReviewsService(reviewRepository);
 
 const reviewsController = new ReviewsController(reviewsService);
 
-router.post('/reviews', authMiddleware, (req, res) =>
-  reviewsController.createReview(req, res)
-);
+router.post('/reviews', authMiddleware, (req, res, next) => {reviewsController.createReview(req, res, next);});
+router.get('/reviews', (req, res, next) => reviewsController.getReviews(req, res, next));
+router.get('/reviews/:id', (req, res, next) => reviewsController.getReviewById(req, res, next));
+router.patch('/reviews/:reviewId', authMiddleware, (req, res, next) => reviewsController.updateReview(req, res, next));
+router.delete('/reviews/:reviewId', authMiddleware, (req, res, next) => reviewsController.deleteReview(req, res, next));
 
-router.get('/reviews', (req, res) => reviewsController.getReviews(req, res));
-
-router.get('/reviews/:reviewId', (req, res) =>
-  reviewsController.getReviewById(req, res)
-);
-
-router.patch('/reviews/:reviewId', authMiddleware, (req, res) =>
-  reviewsController.updateReview(req, res)
-);
-
-router.delete('/reviews/:reviewId', authMiddleware, (req, res) =>
-  reviewsController.deleteReview(req, res)
-);
 
 export default router;
