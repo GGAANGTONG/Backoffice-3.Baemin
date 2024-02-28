22c88724d5a9417963e7c6ccb21f61fb70a22fbc
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-02-28 11:43:23 +0900

    Create users.routes.js
    users.routes.js

---
+++ src/routes/users.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+// import usersController from "../controller/users.controller.js"
+import jwtValidate from "../../middleware/jwt-validate.middleware.js";
+import { redisCache } from "../redis/index.js";
+import { dataSource } from "../typeorm/index.js";
+import { UsersController } from "../controller/users.controller.js";
+import { UsersService } from "../service/users.service.js";
+import { UsersRepository } from "../repository/users.repository.js";
+
+const router = express.Router();
+
+const usersRepository = new UsersRepository(dataSource)
+const usersService = new UsersService(usersRepository, redisCache)
+const usersController = new UsersController(usersService)
+
+router.post('/sign-up', usersController.signUp)
+router.post('/sign-in', usersController.signIn)
+router.get('/get-info', jwtValidate, usersController.getInfo)
+
+export default router
