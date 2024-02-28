79dccfaa5b02794daf368bb8f73a2079ed9e427d
KimGrang <105832184+KimGrang@users.noreply.github.com>
2024-02-28 11:45:07 +0900

    Create auth.controller.js
    auth.controller.js

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import authService from '../service/auth.service.js'
+
+class AuthController {
+    generateNewAccessTokenByFreshToken = async (req, res) => {
+        try {
+            const { refreshToken } = req.body;
+
+            const token = await authService.verifyFreshToken(refreshToken);
+            return res.json(token);
+        } catch (err) {
+            return res.status(err.code).json(err)
+        }
+    }
+}
+
+const authController = new AuthController();
+export default authController;
