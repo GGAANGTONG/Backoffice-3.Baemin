30f626d4b64257114ff25b6ae6966dc673eb6091
cocojerry1 <91053150+cocojerry1@users.noreply.github.com>
2024-02-28 22:32:00 +0900

    Update reviewscontroller.js
    수정

--- src/controllers/reviewscontroller.js
+++ src/controllers/reviewscontroller.js
@@ -1,67 +1,79 @@
-export class ReviewsController {
+export default class ReviewsController {
   constructor(reviewsService) {
     this.reviewsService = reviewsService;
   }
 
-  async createReview(req, res, next) {
-    try {
-      const { userId } = req.user;
-      const { storeId, rating, content } = req.body;
-      const review = await this.reviewsService.createReview({
-        userId,
-        storeId,
-        rating,
-        content,
-      });
-      res.status(201).json({ data: review });
-    } catch (error) {
-      next(error);
-    }
-  }
+async createReview(req, res, next) {
+  try {
 
-  async getReviews(req, res, next) {
-    try {
-      const { storeId } = req.query;
-      const reviews = await this.reviewsService.getReviews({ storeId });
-      res.status(200).json({ data: reviews });
-    } catch (error) {
+      const {  storedId, menuId, rating, content } = req.body; 
+      const user = res.locals.user; console.log(11,user)
+      const createdReview = await this.reviewsService.createReview(
+          menuId,
+          storedId,
+          user.userId,
+          rating,
+          content,
+      );
+      res.status(201).json({ createdReview });
+  } catch (error) {
       next(error);
-    }
   }
+}
 
-  async getReviewById(req, res, next) {
-    try {
-      const { reviewId } = req.params;
+
+async getReviews(req, res, next) {
+  try {
+      const reviews = await this.reviewsService.getReviews();
+      res.status(200).json({ reviews });
+  } catch (error) {
+      next(error);
+  }
+}
+
+async getReviewById(req, res, next) {
+  try {
+      const reviewId = req.params.id; 
+      if (!reviewId) {
+          return res.status(400).json({ error: '리뷰 ID가 제공되지 않았습니다.' });
+      }
       const review = await this.reviewsService.getReviewById(reviewId);
-      res.status(200).json({ data: review });
-    } catch (error) {
+      res.status(200).json(review);
+  } catch (error) {
       next(error);
-    }
   }
+}
 
-  async updateReview(req, res, next) {
-    try {
+
+async updateReview(req, res, next) {
+  try {
       const { reviewId } = req.params;
-      const { rating, content } = req.body;
-      const updatedReview = await this.reviewsService.updateReview(reviewId, {
-        rating,
-        content,
-      });
-      res
-        .status(200)
-        .json({ message: '리뷰가 수정되었습니다.', data: updatedReview });
-    } catch (error) {
+      const updateData = req.body;
+      const updatedReview = await this.reviewsService.updateReview(reviewId, updateData);
+      if (!updatedReview) {
+          return res.status(404).json({ message: '리뷰가없습니다' });
+      }
+      res.status(200).json({ updatedReview });
+  } catch (error) {
       next(error);
-    }
   }
+}
 
-  async deleteReview(req, res, next) {
-    try {
+
+async deleteReview(req, res, next) {
+  try {
       const { reviewId } = req.params;
       await this.reviewsService.deleteReview(reviewId);
-      res.status(200).json({ message: '리뷰가 삭제되었습니다.' });
-    } catch (error) {
+      res.status(204).send(); 
+  } catch (error) {
       next(error);
-    }
   }
+}
+
+
+
+
+
+
+
 }
