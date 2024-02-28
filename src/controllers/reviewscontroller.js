a7f45e19a9ddb2ccd33a546171322d4827fece44
wltjsl <ljss7291@gmail.com>
2024-02-28 22:44:14 +0900

    삭제 시 메시지

--- src/controllers/reviewscontroller.js
+++ src/controllers/reviewscontroller.js
@@ -3,77 +3,72 @@ export default class ReviewsController {
     this.reviewsService = reviewsService;
   }
 
-async createReview(req, res, next) {
-  try {
-
-      const {  storedId, menuId, rating, content } = req.body; 
-      const user = res.locals.user; console.log(11,user)
+  async createReview(req, res, next) {
+    try {
+      const { storedId, menuId, rating, content } = req.body;
+      const user = res.locals.user;
+      console.log(11, user);
       const createdReview = await this.reviewsService.createReview(
-          menuId,
-          storedId,
-          user.userId,
-          rating,
-          content,
+        menuId,
+        storedId,
+        user.userId,
+        rating,
+        content
       );
       res.status(201).json({ createdReview });
-  } catch (error) {
+    } catch (error) {
       next(error);
+    }
   }
-}
 
-
-async getReviews(req, res, next) {
-  try {
+  async getReviews(req, res, next) {
+    try {
       const reviews = await this.reviewsService.getReviews();
       res.status(200).json({ reviews });
-  } catch (error) {
+    } catch (error) {
       next(error);
+    }
   }
-}
 
-async getReviewById(req, res, next) {
-  try {
-      const reviewId = req.params.id; 
+  async getReviewById(req, res, next) {
+    try {
+      const reviewId = req.params.id;
       if (!reviewId) {
-          return res.status(400).json({ error: '리뷰 ID가 제공되지 않았습니다.' });
+        return res
+          .status(400)
+          .json({ error: '리뷰 ID가 제공되지 않았습니다.' });
       }
       const review = await this.reviewsService.getReviewById(reviewId);
       res.status(200).json(review);
-  } catch (error) {
+    } catch (error) {
       next(error);
+    }
   }
-}
 
-
-async updateReview(req, res, next) {
-  try {
+  async updateReview(req, res, next) {
+    try {
       const { reviewId } = req.params;
       const updateData = req.body;
-      const updatedReview = await this.reviewsService.updateReview(reviewId, updateData);
+      const updatedReview = await this.reviewsService.updateReview(
+        reviewId,
+        updateData
+      );
       if (!updatedReview) {
-          return res.status(404).json({ message: '리뷰가없습니다' });
+        return res.status(404).json({ message: '리뷰가없습니다' });
       }
       res.status(200).json({ updatedReview });
-  } catch (error) {
+    } catch (error) {
       next(error);
+    }
   }
-}
 
-
-async deleteReview(req, res, next) {
-  try {
+  async deleteReview(req, res, next) {
+    try {
       const { reviewId } = req.params;
       await this.reviewsService.deleteReview(reviewId);
-      res.status(204).send(); 
-  } catch (error) {
+      res.status(204).json({ message: '성공적으로 삭제되었습니다.' });
+    } catch (error) {
       next(error);
+    }
   }
-}
-
-
-
-
-
-
-
 }
--- src/routes/reviews.router.js
+++ src/routes/reviews.router.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import authMiddleware from '../../middleware/jwt-validate.middleware.js';
-import  ReviewsRepository from '../repositories/reivewsrepsitory.js';
-import  ReviewsController  from '../controllers/reviewscontroller.js';
-import  ReviewsService  from '../services/reviewsservice.js';
+import ReviewsRepository from '../repositories/reviewsrepository.js';
+import ReviewsController from '../controllers/reviewscontroller.js';
+import ReviewsService from '../services/reviewsservice.js';
 import { dataSource } from '../typeorm/index.js';
 
 const router = express.Router();
@@ -13,11 +13,20 @@ const reviewsService = new ReviewsService(reviewRepository);
 
 const reviewsController = new ReviewsController(reviewsService);
 
-router.post('/reviews', authMiddleware, (req, res, next) => {reviewsController.createReview(req, res, next);});
-router.get('/reviews', (req, res, next) => reviewsController.getReviews(req, res, next));
-router.get('/reviews/:id', (req, res, next) => reviewsController.getReviewById(req, res, next));
-router.patch('/reviews/:reviewId', authMiddleware, (req, res, next) => reviewsController.updateReview(req, res, next));
-router.delete('/reviews/:reviewId', authMiddleware, (req, res, next) => reviewsController.deleteReview(req, res, next));
-
+router.post('/reviews', authMiddleware, (req, res, next) => {
+  reviewsController.createReview(req, res, next);
+});
+router.get('/reviews', (req, res, next) =>
+  reviewsController.getReviews(req, res, next)
+);
+router.get('/reviews/:id', (req, res, next) =>
+  reviewsController.getReviewById(req, res, next)
+);
+router.patch('/reviews/:reviewId', authMiddleware, (req, res, next) =>
+  reviewsController.updateReview(req, res, next)
+);
+router.delete('/reviews/:reviewId', authMiddleware, (req, res, next) =>
+  reviewsController.deleteReview(req, res, next)
+);
 
 export default router;
